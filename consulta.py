4d97a0826119ee03b1383d714db091c0b8a96284
NachoVidondo <ignaciovidondo@hotmail.com>
2020-11-20 10:13:45 -0300

    Menu

--- consulta.py
+++ consulta.py
@@ -1,8 +1,9 @@
 import sqlite3
 import os
 
+
 APP_PATH=os.getcwd()
-DB_PATH = APP_PATH + "\GastosDB\gastosdatabase.db"
+DB_PATH = APP_PATH + "/gastosdatabase.db"
 
 class Consult():
     def __init__(self):
@@ -12,16 +13,14 @@ class Consult():
         
     def insertData(self,savedTime,textAlquiler,textAuto,textSupermercado,textServicios,textTarjetaCredito,textTransporte,textObraSocial,textRecreativos,textIngresoMama,textIngresoPapa):
          print ('debug insert', self.con, self.cursor)
-         self.cursor.execute("""INSERT INTO Alquiler VALUES(?, ?, ?)""", (None,textAlquiler,savedTime))
-         self.cursor.execute("""INSERT INTO Automovil VALUES(?, ?, ?)""", (None,textAuto,savedTime))
-         self.cursor.execute("""INSERT INTO Obra_Social VALUES(?, ?, ?)""", (None,textObraSocial,savedTime))
-         self.cursor.execute("""INSERT INTO Recreativos VALUES(?, ?, ?)""", (None,textRecreativos,savedTime))
-         self.cursor.execute("""INSERT INTO Servicios VALUES(?, ?, ?)""", (None,textServicios,savedTime))
-         self.cursor.execute("""INSERT INTO Supermercado VALUES(?, ?, ?)""", (None,textSupermercado,savedTime))
-         self.cursor.execute("""INSERT INTO Tarjeta_Credito VALUES(?, ?, ?)""", (None,textTarjetaCredito,savedTime))
-         self.cursor.execute("""INSERT INTO Transporte VALUES(?, ?, ?)""", (None,textTransporte,savedTime))
-         self.cursor.execute("""INSERT INTO Ingreso_Mama VALUES(?, ?, ?)""", (None,textIngresoMama,savedTime))
-         self.cursor.execute("""INSERT INTO Ingreso_Papa VALUES(?, ?, ?)""", (None,textIngresoPapa,savedTime))
+         self.cursor.execute("""INSERT INTO Gastos VALUES(?, ?, ?, ?, ?, ? ,? ,? ,?)""", (textAlquiler,textAuto,textSupermercado,textServicios,textTarjetaCredito,textTransporte,textObraSocial,textRecreativos,savedTime))
+         self.cursor.execute("""INSERT INTO Ingresos VALUES(?, ?, ?)""", (textIngresoMama,textIngresoPapa,savedTime))
          self.con.commit()
          self.con.close()
+ 
+   
+    
+
+
+            
             
--- gastos.py
+++ gastos.py
@@ -1,10 +1,12 @@
 from tkinter import*
 from datetime import datetime
+from tkinter.ttk import Treeview
 from consulta import *
 import time
 import locale
 import sqlite3
 from tkinter import messagebox
+from tkinter import ttk
 
 ventana = Tk()
 ventana.geometry("1200x600")
@@ -24,9 +26,8 @@ def persist(savedTime,textAlquiler,textAuto,textSupermercado,textServicios,textT
     dbConnection = Consult()
     print("Conexion : ", dbConnection.con)
     dbConnection.insertData(savedTime,textAlquiler,textAuto,textSupermercado,textServicios,textTarjetaCredito,textTransporte,textObraSocial,textRecreativos,textIngresoMama,textIngresoPapa)
+
     
-
-
 def enviarDatos():
     textAlquiler = text_Alquiler.get()
     textAuto = text_Auto.get()
@@ -41,8 +42,7 @@ def enviarDatos():
     savedTime = currentDate + "" +  currentTime
     messagebox.showinfo("Nuevos Datos Ingresados","ok")
     persist(savedTime,textAlquiler,textAuto,textSupermercado,textServicios,textTarjetaCredito,textTransporte,textObraSocial,textRecreativos,textIngresoMama,textIngresoPapa)
-    
-    
+
 def nuevoIngreso():
     text_IngMama.config(state="normal")
     text_IngPapa.config(state='normal')
@@ -71,7 +71,6 @@ def deleteIngresos():
     text_IngMama.delete(0, "end")
     text_IngPapa.delete(0, "end")
     
-
 imagen=PhotoImage(file="economia1.png")
 label_imagen=Label(ventana,image=imagen)
 label_imagen.place(x = 0, y = 0)
@@ -79,136 +78,152 @@ label_imagen.pack()
 
 label_EconomiaFamiliar=Label(ventana,text="Economia Familiar")
 label_EconomiaFamiliar.pack()
-label_EconomiaFamiliar.config(fg="#585858",bg="white", fon = ("Goudy Stout",12))
+label_EconomiaFamiliar.config(fg="black",bg="white", fon = ("Century Gothic",25))
 label_EconomiaFamiliar.place(x = 350, y =0)
 
-label_IngresoMama=Label(ventana,text="Ingresos Mama --> ")
+label_CurrentDate = Label(ventana,text=currentDate) 
+label_CurrentDate.pack()
+label_CurrentDate.config(fg="black",bg="white", fon = ("Century Gothic", 15))
+label_CurrentDate.place(x = 900, y =0)
+
+label_IngresoMama=Label(ventana,text="Ingresos Mama")
 label_IngresoMama.pack()
-label_IngresoMama.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_IngresoMama.place(x=170, y = 100)
+label_IngresoMama.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_IngresoMama.place(x=150, y = 100)
 
 
-label_IngresoPapa=Label(ventana,text="Ingresos Papa --> ")
+label_IngresoPapa=Label(ventana,text="Ingresos Papa")
 label_IngresoPapa.pack()
-label_IngresoPapa.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_IngresoPapa.place(x=170, y = 150)
+label_IngresoPapa.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_IngresoPapa.place(x=150, y = 150)
 
 label_Ingresos=Label(ventana,text="INGRESOS ")
 label_Ingresos.pack()
-label_Ingresos.config(fg="white",bg="#0B3B24", fon = ("Century Gothic",15))
-label_Ingresos.place(x = 20, y = 130)
+label_Ingresos.config(fg="white",bg="black", fon = ("Century Gothic",10))
+label_Ingresos.place(x = 10, y = 120)
 
 label_Gastos=Label(ventana,text="GASTOS ")
 label_Gastos.pack()
-label_Gastos.config(fg="white",bg="#0B3B24", fon = ("Century Gothic",15))
-label_Gastos.place(x = 150, y = 130)
+label_Gastos.config(fg= "white",bg="black", fon = ("Century Gothic",10))
+label_Gastos.place(x = 10, y = 400)
 
 
-label_Alquiler=Label(ventana,text="Alquiler -->")
+label_Alquiler=Label(ventana,text="Alquiler ")
 label_Alquiler.pack()
-label_Alquiler.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_Alquiler.place(x=400, y = 100)
+label_Alquiler.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_Alquiler.place(x=150, y = 300)
 
-label_Auto=Label(ventana,text="Automovil --> ")
+label_Auto=Label(ventana,text="Automovil")
 label_Auto.pack()
-label_Auto.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_Auto.place(x=400, y = 150)
+label_Auto.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_Auto.place(x=150, y = 350)
 
-label_Supermercado=Label(ventana,text="Supermercado --> ")
+label_Supermercado=Label(ventana,text="Supermercado")
 label_Supermercado.pack()
-label_Supermercado.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_Supermercado.place(x=400, y = 200)
+label_Supermercado.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_Supermercado.place(x=150, y = 400)
 
-label_Servicios=Label(ventana,text="Servicios --> ")
+label_Servicios=Label(ventana,text="Servicios")
 label_Servicios.pack()
-label_Servicios.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_Servicios.place(x=400, y = 250)
+label_Servicios.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_Servicios.place(x=150, y = 450)
 
-label_TarjetaCredito=Label(ventana,text="Tarjeta de Credito --> ")
+label_TarjetaCredito=Label(ventana,text="Tarjeta de Credito")
 label_TarjetaCredito.pack()
-label_TarjetaCredito.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_TarjetaCredito.place(x=400, y = 300)
+label_TarjetaCredito.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_TarjetaCredito.place(x=450, y = 300)
 
-label_Transporte=Label(ventana,text="Transporte --> ")
+label_Transporte=Label(ventana,text="Transporte")
 label_Transporte.pack()
-label_Transporte.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_Transporte.place(x=400, y = 350)
+label_Transporte.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_Transporte.place(x=450, y = 350)
 
-label_ObraSocial=Label(ventana,text="Obra Social --> ")
+label_ObraSocial=Label(ventana,text="Obra Social")
 label_ObraSocial.pack()
-label_ObraSocial.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_ObraSocial.place(x=400, y = 400)
+label_ObraSocial.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_ObraSocial.place(x=450, y = 400)
 
 
-label_Recreativos=Label(ventana,text="Recreativos --> ")
+label_Recreativos=Label(ventana,text="Recreativos")
 label_Recreativos.pack()
-label_Recreativos.config(fg="black",bg="white",fon = ("Agency FB", 15))
-label_Recreativos.place(x=400, y = 450)
+label_Recreativos.config(fg="black",bg="white",fon = ("Century Gothic", 10))
+label_Recreativos.place(x=450, y = 450)
 
 
 text_IngMama=Entry(ventana)
 text_IngMama.config(state='readonly')
-text_IngMama.place(x=300, y = 105)
+text_IngMama.place(x=270, y = 105)
 
 text_IngPapa=Entry(ventana)
 text_IngPapa.config(state='readonly')
-text_IngPapa.place(x=300, y = 155)
+text_IngPapa.place(x=270, y = 155)
 
 text_Alquiler=Entry(ventana)
 text_Alquiler.config(state='readonly')
-text_Alquiler.place(x=550, y = 105)
+text_Alquiler.place(x=270, y = 305)
 
 text_Auto=Entry(ventana)
 text_Auto.config(state='readonly')
-text_Auto.place(x=550, y = 155)
+text_Auto.place(x=270, y = 355)
 
 text_Supermercado=Entry(ventana)
 text_Supermercado.config(state='readonly')
-text_Supermercado.place(x=550, y = 205)
+text_Supermercado.place(x=270, y = 405)
 
 text_Servicios=Entry(ventana)
 text_Servicios.config(state='readonly')
-text_Servicios.place(x=550, y = 255)
+text_Servicios.place(x=270, y = 455)
 
 text_TarjetaCredito=Entry(ventana)
 text_TarjetaCredito.config(state='readonly')
-text_TarjetaCredito.place(x=550, y = 305)
+text_TarjetaCredito.place(x=590, y = 305)
 
 text_Transporte=Entry(ventana)
 text_Transporte.config(state='readonly')
-text_Transporte.place(x=550, y = 355)
+text_Transporte.place(x=590, y = 365)
 
 text_ObraSocial=Entry(ventana)
 text_ObraSocial.config(state='readonly')
-text_ObraSocial.place(x=550, y = 405)
+text_ObraSocial.place(x=590, y = 405)
 
 text_Recreativos=Entry(ventana)
 text_Recreativos.config(state='readonly')
-text_Recreativos.place(x=550, y = 455)
+text_Recreativos.place(x=590, y = 455)
 
-botonNuevoGasto=Button(ventana, text="Nuevos Gastos",command = nuevoGasto)
-botonNuevoGasto.place(x = 300, y = 550)
-botonNuevoGasto.config(bg="#2EFEF7", fg= "black")
+botonNuevoGasto=Button(ventana, text="Nuevos  Gastos",command = nuevoGasto)
+botonNuevoGasto.place(x = 100, y = 550)
+botonNuevoGasto.config(bg="white", fg= "black")
 
-botonGuardarGastos=Button(ventana,text="Guardar Gastos",command=enviarDatos)
-botonGuardarGastos.place(x = 450, y = 550)
-botonGuardarGastos.config(bg="#848484", fg= "black")
+botonGuardarGastos=Button(ventana,text="Guardar  Gastos",command=enviarDatos)
+botonGuardarGastos.place(x = 250, y = 550)
+botonGuardarGastos.config(bg="white", fg= "black")
+
+botonBorrarGastos=Button(ventana,text="Borrar  Gastos",command=deleteGastos) 
+botonBorrarGastos.place (x = 400, y = 550)
+botonBorrarGastos.config(bg="white", fg= "black")
 
 botonNuevoIngreso=Button(ventana, text="Nuevo Ingreso",  command = nuevoIngreso)
-botonNuevoIngreso.place(x = 200, y = 250)
-botonNuevoIngreso.config(bg="#2EFEF7", fg= "black")
+botonNuevoIngreso.place(x = 100, y = 230)
+botonNuevoIngreso.config(bg="white", fg= "black")
 
 botonGuardarIngresos=Button(ventana,text="Guardar Ingresos",command=enviarDatos)
-botonGuardarIngresos.place(x = 350, y = 250)
-botonGuardarIngresos.config(bg="#848484", fg= "black")
+botonGuardarIngresos.place(x = 250, y = 230)
+botonGuardarIngresos.config(bg="white", fg= "black")
 
 botonBorrarIngresos=Button(ventana,text="Borrar Ingresos",command=deleteIngresos)
-botonBorrarIngresos.place(x=450, y= 250 )
-botonBorrarIngresos.config(bg="#FF0000", fg= "black")
+botonBorrarIngresos.place(x=400, y= 230 )
+botonBorrarIngresos.config(bg="white", fg= "black")
 
-botonBorrarGastos=Button(ventana,text="Borrar Gastos",command=deleteGastos) 
-botonBorrarGastos.place (x = 600, y = 550)
-botonBorrarGastos.config(bg="#FF0000", fg= "black")
+menubar=Menu(ventana)
+ventana.config(menu=menubar)
+Mostrarmenu=Menu(menubar,tearoff=0)
+menubar.add_cascade(label="Menu ", menu= Mostrarmenu)
 
+Mostrarmenu.add_command(label="Abrir Ingresos")
+Mostrarmenu.add_separator()
+
+
+Mostrarmenu.add_command(label="Abrir Gastos")
+Mostrarmenu.add_separator()
 
 ventana.mainloop()
